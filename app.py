0766322db45785fd77b8e59448fed7dd39590668
Amarkdevops <kumar.amar786@gmail.com>
2025-04-07 20:49:27 +0530

    i have updated app.py file

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
-{
+"resource": "/{proxy+}",
+  "path": "/hello",
+  "httpMethod": "GET",
+  "headers": { ... },
+  "queryStringParameters": { "name": "John" },
+  "pathParameters": { "proxy": "/hello" },
+  "body": null,
+  "isBase64Encoded": false
+"resource": "/{proxy+}",
+  "path": "/hello",
+  "httpMethod": "GET",
+  "headers": { ... },
+  "queryStringParameters": { "name": "John" },
+  "pathParameters": { "proxy": "/hello" },
+  "body": null,
+  "isBase64Encoded": false
+}
+
   "resource": "/{proxy+}",
+  "path": "/hello",
+  "httpMethod": "GET",
+  "headers": { ... },
+  "queryStringParameters": { "name": "John" },
+  "pathParameters": { "proxy": "/hello" },
+  "body": null,
+  "isBase64Encoded": false
+"resource": "/{proxy+}",
   "path": "/hello",
   "httpMethod": "GET",
   "headers": { ... },
